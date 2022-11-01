303bf6babeeb3b501d015897dcf3854a9e8437ac
cadusds <carlos.silva150995@gmail.com>
2022-11-01 10:44:10 -0300

    add test to Game.info()

--- ex_mon2/test/game_test.exs
+++ ex_mon2/test/game_test.exs
@@ -11,4 +11,38 @@ defmodule ExMon2.GameTest do
       assert {:ok, _pid} = Game.start(computer,player)
     end
   end
+
+
+  describe "info/0" do
+    test "returns the current game state" do
+      player = Player.build("cadu",:chute,:soco,:cura)
+      Player.build("Robozão",:chute,:soco,:cura)
+      |> Game.start(player)
+
+      expected_response = %{
+        computer: %Player{
+          life: 100,
+          moves: %{
+            move_avg: :soco,
+            move_heal: :cura,
+            move_random: :chute
+          },
+          name: "Robozão"
+        },
+        player: %Player{
+          life: 100,
+          moves: %{
+            move_avg: :soco,
+            move_heal: :cura,
+            move_random: :chute
+          },
+          name: "cadu"
+        },
+        status: :started,
+        turn: :player
+      }
+      assert Game.info() == expected_response
+
+    end
+  end
 end
